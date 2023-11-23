464994cfd52962f1d1bde00083e6ffa9e3648b90
Sergio Fernandez Davila <killeranime666@gmail.com>
2023-11-22 22:30:02 -0400

    Modificada BDD, agregados historicos de propietarios y pujas y agregados los triggers de llenado de los historicos

--- Auctions.sql
+++ Auctions.sql
@@ -1,40 +1,41 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2023-11-09 18:48:34.617
+-- Last modification date: 2023-11-23 01:29:53.211
 
 -- tables
 -- Table: Auctions
 CREATE TABLE Auctions (
     idAuction serial  NOT NULL,
+    Users_idUser int  NOT NULL,
+    Cards_idCard int  NOT NULL,
+    City_idCity int  NOT NULL,
     startDate time  NOT NULL,
     endDate time  NULL,
     Price decimal(10,2)  NOT NULL,
     isSell boolean  NOT NULL,
     isCancelled boolean  NOT NULL,
-    Cards_cardCode char(32)  NOT NULL,
-    Users_idUser int  NOT NULL,
-    City_idCity int  NOT NULL,
     CONSTRAINT Auctions_pk PRIMARY KEY (idAuction)
 );
 
 -- Table: Bids
 CREATE TABLE Bids (
     idBid serial  NOT NULL,
+    Auctions_idAuction int  NOT NULL,
+    Users_idUser int  NOT NULL,
     ammount decimal(10,2)  NOT NULL,
     date time  NOT NULL,
     isWinner boolean  NOT NULL,
-    Auctions_idAuction int  NOT NULL,
-    Users_idUser int  NOT NULL,
     CONSTRAINT Bids_pk PRIMARY KEY (idBid)
 );
 
 -- Table: Cards
 CREATE TABLE Cards (
+    idCard serial  NOT NULL,
     cardCode char(32)  NOT NULL,
     rarity char(64)  NOT NULL,
     name char(128)  NOT NULL,
     imgLink char(32)  NOT NULL,
     marketPrice decimal(10,2)  NOT NULL,
-    CONSTRAINT Cards_pk PRIMARY KEY (cardCode)
+    CONSTRAINT Cards_pk PRIMARY KEY (idCard)
 );
 
 -- Table: City
@@ -44,11 +45,32 @@ CREATE TABLE City (
     CONSTRAINT City_pk PRIMARY KEY (idCity)
 );
 
+-- Table: H_Bids
+CREATE TABLE H_Bids (
+    h_id_Bids serial  NOT NULL,
+    Bids_idBids int  NOT NULL,
+    User_idUser int  NOT NULL,
+    ammount decimal(10,2)  NOT NULL,
+    date time  NOT NULL,
+    isWinner boolean  NOT NULL,
+    CONSTRAINT H_Bids_pk PRIMARY KEY (h_id_Bids)
+);
+
+-- Table: H_Owners
+CREATE TABLE H_Owners (
+    id_h_owners serial  NOT NULL,
+    Owners_idOwners int  NOT NULL,
+    User_idUser int  NOT NULL,
+    Cards_cardCode int  NOT NULL,
+    acquired time  NOT NULL,
+    CONSTRAINT H_Owners_pk PRIMARY KEY (id_h_owners)
+);
+
 -- Table: Images
 CREATE TABLE Images (
     idImages serial  NOT NULL,
-    imgUpload char(64)  NOT NULL,
     Auctions_idAuction int  NOT NULL,
+    imgUpload char(64)  NOT NULL,
     CONSTRAINT Images_pk PRIMARY KEY (idImages)
 );
 
@@ -56,7 +78,7 @@ CREATE TABLE Images (
 CREATE TABLE Owners (
     idOwner serial  NOT NULL,
     Users_idUser int  NOT NULL,
-    Cards_cardCode char(32)  NOT NULL,
+    Cards_idCard int  NOT NULL,
     acquired time  NOT NULL,
     CONSTRAINT Owners_pk PRIMARY KEY (idOwner)
 );
@@ -71,8 +93,8 @@ CREATE TABLE Users (
 -- foreign keys
 -- Reference: Auctions_Cards (table: Auctions)
 ALTER TABLE Auctions ADD CONSTRAINT Auctions_Cards
-    FOREIGN KEY (Cards_cardCode)
-    REFERENCES Cards (cardCode)  
+    FOREIGN KEY (Cards_idCard)
+    REFERENCES Cards (idCard)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
@@ -119,8 +141,8 @@ ALTER TABLE Images ADD CONSTRAINT Images_Auctions
 
 -- Reference: Owners_Cards (table: Owners)
 ALTER TABLE Owners ADD CONSTRAINT Owners_Cards
-    FOREIGN KEY (Cards_cardCode)
-    REFERENCES Cards (cardCode)  
+    FOREIGN KEY (Cards_idCard)
+    REFERENCES Cards (idCard)  
     NOT DEFERRABLE 
     INITIALLY IMMEDIATE
 ;
@@ -134,4 +156,35 @@ ALTER TABLE Owners ADD CONSTRAINT Owners_Users
 ;
 
 -- End of file.
+
+-- Triggers
+-- Trigger: trigger_h_owners
+CREATE OR REPLACE FUNCTION trigger_h_bids()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO H_Bids (Bids_idBids, User_idUser, ammount, date, isWinner)
+    VALUES (NEW.idBid, NEW.Users_idUser, NEW.ammount, NEW.date, NEW.isWinner);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER h_bids_trigger
+AFTER INSERT ON Bids
+FOR EACH ROW
+EXECUTE FUNCTION trigger_h_bids();
+
+-- Trigger: trigger_h_owners
+CREATE OR REPLACE FUNCTION trigger_h_owners()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO H_Owners (Owners_idOwners, User_idUser, Cards_cardCode, acquired)
+    VALUES (NEW.idOwner, NEW.Users_idUser, NEW.Cards_idCard, NEW.acquired);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER h_owners_trigger
+AFTER INSERT ON Owners
+FOR EACH ROW
+EXECUTE FUNCTION trigger_h_owners();
 
